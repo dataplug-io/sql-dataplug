f19a785a0e6c9a00a2a22b1e4c3edb9b958cfcf5
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-18 10:55:16 +0300

    Dependencies

---
+++ test/index.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const Source = require('../lib');
+
+chai.should();
+
+describe('dataplug-cli', () => {
+  describe('#()', () => {
+    it('', () => {});
+  });
+
+  describe('#createOutput()', () => {
+    it('', () => {});
+  });
+});
