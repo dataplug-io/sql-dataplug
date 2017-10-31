c3a1307409a7858331262d3c716695d0e5976fa1
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-31 19:18:14 +0200

    Basic SQL schema

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const path = require('path')
+
 require('@dataplug/dataplug-cli').build()
   .usingCollectionFactory(require('../lib/factory'))
+  .usingCommandsFromDir(path.join(__dirname, '../lib/commands'))
   .process()
