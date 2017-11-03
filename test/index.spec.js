945684c1c0766b969eb7360f2de3e53cb66970e6
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-03 09:06:37 +0200

    Refactor; test suite

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,11 @@
 /* eslint-env node, mocha */
 require('chai')
   .should()
+const dataplugTestsuite = require('@dataplug/dataplug-testsuite')
+const sqlDataplug = require('../lib')
+
+describe('sql-dataplug', () => {
+  dataplugTestsuite
+    .forCollectionFactory('sql', sqlDataplug.factory)
+    .use()
+})
